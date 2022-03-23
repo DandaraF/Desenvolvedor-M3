6ba9a83e9cefa332febf704aafc59ba1cf6784cf
DandaraF <dandara-chr@hotmail.com>
2022-03-23 14:51:16 -0300

    replace dot with comma and add two decimal places of commas

--- src/js/index.js
+++ src/js/index.js
@@ -16,19 +16,28 @@ function getProducts() {
 
 function listProducts() {
   products = getProducts();
+  console.log(products);
   const teste = products.map((produto, index) => {
     return `
     <div class="card-product">
       <img class="img-product" src=".${produto.image}"/>
       <p class="name-product">${produto.name}</p>
-      <p class="price-product">R$ ${produto.price}</p>
-      <p class="share">até ${produto.parcelamento[0]}x de R${produto.parcelamento[1]}</p>
+      <p class="price-product">R$ ${replaceSeparator(
+        produto.price.toFixed(2)
+      )}</p>
+      <p class="share">até ${produto.parcelamento[0]}x de R$${replaceSeparator(
+      produto.parcelamento[1].toFixed(2)
+    )}</p>
       <button class="btn-buy">COMPRAR</button> 
     </div>
 
     `;
   });
   document.querySelector(".container-products").innerHTML = teste.join("");
+}
+
+function replaceSeparator(price) {
+  return price.toString().replace(".", ",");
 }
 
 listProducts();
